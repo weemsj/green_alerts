0a263457342ae00d1afd296d48a39e0bd53d32ae
TroyPeele2 <TroyPeele2@users.noreply.github.com>
2021-09-17 15:16:29 -0700

    added components folder w/ header

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
