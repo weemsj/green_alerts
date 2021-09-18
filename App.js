6bcd7e06d45251bd606cb61983293b0ba8e01f1c
weemsj <71594052+weemsj@users.noreply.github.com>
2021-09-19 01:57:06 +0200

    -

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, Platform  } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Header from './components/header';
@@ -82,26 +82,41 @@ export default function App() {
   }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}>
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Title: {notification && notification.request.content.title} </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+    <View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen
+            name='Home'
+            component={HomeScreen}
+            options={{ title: 'Landing Page' }}
+          />
+          <Stack.Screen name='Config' component={Configuration} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        }}>
+        <Text>Your expo push token: {expoPushToken}</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Title: {notification && notification.request.content.title} </Text>
+          <Text>Body: {notification && notification.request.content.body}</Text>
+          <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+        </View>
+        <Button
+          title="Press to schedule a Notification"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+        />
       </View>
-      <Button
-        title="Press to schedule a Notification"
-        onPress={async () => {
-          await schedulePushNotification();
-        }}
-      />
     </View>
+
   );
+
+
 }
 
 // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
