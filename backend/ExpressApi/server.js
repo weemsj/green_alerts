b38cc1c482657a41b1e2240c87fb0edacf98fe99
Milan Donhowe <milanaugust@gmail.com>
2021-09-18 17:47:15 -0700

    basic express backend up & running

---
+++ backend/ExpressApi/server.js
@@ -0,0 +1,72 @@
+/*
+  Filename: server.js
+  Description: 
+    Dead-simple express JSON server to store user data. 
+    This was made in a few hours for a hackathon and is in no ways intended
+    for *actual* production use or storing sensitive user data.
+*/
+
+const express = require("express")
+const app = express()
+const path = require("path")
+const fs = require('fs')
+
+const DATASTORE_FILENAME = 'datastore.json'
+
+// even in a hackathon server under crunch time I refuse to store plaintext passwords
+const HASH = (pwd) => require('crypto').createHash('md5').update(pwd).digest("hex")
+
+app.use(express.json())
+
+// Read JSON data from file
+const dataStore = JSON.parse(fs.readFileSync(DATASTORE_FILENAME,{encoding:'utf8', flag:'r'}))
+
+// in case we need to reset the file
+function resetFile() {
+  fs.writeFileSync(DATASTORE_FILENAME, JSON.stringify({
+    'users': {},
+    'notifications': {}
+  }))
+}
+
+
+// add user data (just name, password, email for now...)
+app.post("/addUser", (req, res) => {
+
+  const {name, password, email} = req.body
+
+  if (Object.keys(dataStore.users).includes(name)) {
+    return res.status(400).send('User already exists.')
+  }
+
+  // Added to database (magic!)
+  dataStore.users[name] = {password: HASH(password), email}
+  
+  // doing a blocking file write here because I hate the 
+  // cloud and I want it this VM to suffer
+  fs.writeFileSync(DATASTORE_FILENAME, JSON.stringify(dataStore))
+
+  res.status(200).send('User added')
+})
+
+// Return User Data
+app.get("/getUser", (req, res) => {
+  const { name, password } = req.body
+
+  if (dataStore.users[name]) {
+    if (dataStore.users[name].password === HASH(password)) {
+      // found user, return data
+      return res.status(200).send(JSON.stringify({
+        name: name,
+        email: dataStore.users[name].email
+      }))
+    }
+  }
+
+  return res.status(400).send('User does not exist and/or bad credentials')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,
+  () => console.log(`Server Running on Port ${PORT}`)
+)
---
+++ backend/ExpressApi/test.js
@@ -0,0 +1,65 @@
+/*
+  Filename: test.js
+  Description:
+    basic sanity HTTP tests to make sure the express
+    server logic kinda-sorta-works. 
+*/
+
+const axios = require('axios')
+
+const HOST = 'localhost'
+const PORT = 5000
+const ENDPOINT = `http://${HOST}:${PORT}`
+
+const fakeUsers = [
+  {name: 'AAA', email: 'A@A.net', password: 'AAA'},
+  {name: 'BBB', email: 'B@B.org', password: 'BBB'},
+  {name: 'CCC', email: 'whitehouse.gov', password: 'CH#WR'}
+]
+
+async function runTests () {
+  console.log(`Running tests on ${ENDPOINT}`)
+
+
+  // Check add User!
+  /*
+  axios ( {
+    method: 'post',
+    url: ENDPOINT + '/addUser',
+    data: fakeUsers[2]
+  }).then(res => {
+    console.log(`Response should be 200: actually is ${res.status}`)
+    console.log(`Response reads: ${res.data}`)
+  }).catch(res => {
+    console.log(res.status)
+    console.log(res.data)
+    console.log('fail--maybe already added?')
+  })
+
+  // Check duplicate user add!
+  axios ( {
+    method: 'post',
+    url: ENDPOINT + '/addUser',
+    data: fakeUsers[1]
+  }).catch(res => {
+    console.log(`Response should be 400: actually is ${res.status}`)
+    console.log(`Response reads: ${res.data}`)
+  })
+  */
+  // Check get User 
+  axios ( {
+    method: 'GET',
+    url: ENDPOINT + '/getUser',
+    data: { name: fakeUsers[0].name, password: fakeUsers[0].password }
+  }).then(res => {
+    console.log(`Response should be 200: actually is ${res.status}`)
+    console.log(res.data)
+  }).catch(err => {
+    console.log(`we got an unexpected error: ${err}`)
+  })
+
+
+
+}
+
+runTests()
